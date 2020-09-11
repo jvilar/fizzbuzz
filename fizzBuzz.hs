6818f8eaa0d915c0b32c41e166fd4ff2c6d907d3
Juan Miguel Vilar <jvilar@uji.es>
2020-09-11 10:44:14 +0200

    Generalized

--- fizzBuzz.hs
+++ fizzBuzz.hs
@@ -4,11 +4,9 @@ main :: IO ()
 main = mapM_ putStrLn $ map translate [1..100]
 
 translate :: Int -> String
-translate n | isFizz && isBuzz = "FizzBuzz"
-            | isFizz = "Fizz"
-            | isBuzz = "Buzz"
-            | otherwise = show n
-            where isFizz = check 3 n
-                  isBuzz = check 5 n
+translate n | null c = show n
+            | otherwise = c
+            where c = concat [ str | (cnd, str) <- checks, cnd n ]
                   check b n = n `rem` b == 0 || has (intToDigit b) n
                   has c = (elem c) . show
+                  checks = [(check 3, "Fizz"), (check 5, "Buzz"), (check 7, "Tazz")]
